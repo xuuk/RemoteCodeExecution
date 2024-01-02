a47e01dc5e9277274ef5800fe7a60f39b41c6fa0
Orion <11402085+q826663093@user.noreply.gitee.com>
2024-01-02 11:27:14 +0800

    2024-1-2

---
+++ src/main/java/com/rce/annotation/DoThisMethod.java
@@ -0,0 +1,16 @@
+package com.rce.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author xuke
+ * @create 2023/12/29
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface DoThisMethod {
+    int priority() default 1;
+}
